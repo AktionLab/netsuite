5379e55ebd08c541056537bc84a2920a79aa4d59
Daniel Zukowski <daniel.zukowski@gmail.com>
2013-09-18 23:10:14 -0600

    records for handling Purchase Orders

---
+++ lib/netsuite/records/purchase_order.rb
@@ -0,0 +1,36 @@
+module NetSuite
+  module Records
+    class PurchaseOrder
+      include Support::Fields
+      include Support::RecordRefs
+      include Support::Records
+      include Support::Actions
+      include Namespaces::TranPurch
+
+      actions :get, :add, :initialize, :delete, :update, :search
+
+      field :transaction_bill_address, BillAddress
+      field :transaction_ship_address, ShipAddress
+      field :custom_field_list, CustomFieldList
+
+      #field :expense_list, PurchaseOrderExpenseList
+      field :item_list, PurchaseOrderItemList
+      #field :order_status, PurchaseOrderOrderStatus
+
+      record_refs :approval_status, :bill_address_list, :klass, :created_from, :currency, :custom_form, :department, :employee, :entity, :interco_transaction, :location, :next_approver, :ship_address_list, :ship_method, :ship_to, :subsidiary, :terms
+
+      fields :bill_address, :created_date, :currency_name, :due_date, :email, :exchange_rate, :fax, :fob, :last_modified_date, :linked_tracking_numbers, :memo, :message, :other_ref_num, :ship_address, :ship_date, :source, :status, :sub_total, :supervisor_approval, :tax_2_total, :tax_total, :to_be_emailed, :to_be_faxed, :to_be_printed, :total, :tracking_numbers, :tran_date, :tran_id, :vat_reg_num
+
+      attr_reader :internal_id
+      attr_accessor :external_id
+      attr_accessor :search_joins
+
+      def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
+        initialize_from_attributes_hash(attributes)
+      end
+
+    end
+  end
+end
---
+++ lib/netsuite/records/purchase_order_item.rb
@@ -0,0 +1,42 @@
+module NetSuite
+  module Records
+    class PurchaseOrderItem
+      include Support::Fields
+      include Support::RecordRefs
+      include Support::Records
+      include Namespaces::TranPurch
+
+      field :inventory_detail, InventoryDetail
+      field :custom_field_list, CustomFieldList
+      field :options, CustomFieldList
+
+      record_refs :klass, :customer, :department, :item, :landed_cost_category, :location, :tax_code, :units platformCore:RecordRef
+
+      #field :bill_variance_status, TransactionBillVarianceStatus
+
+      fields :amount, :description, :expected_receipt_date, :gross_amt, :is_billable, :is_closed, :line, :match_bill_to_receipt, :quantity, :quantity_available, :quantity_billed, :quantity_on_hand, :quantity_received, :rate, :serial_numbers, :tax_1_amt, :tax_rate_1, :tax_rate_2, :vendor_name
+
+      def initialize(attributes_or_record = {})
+        case attributes_or_record
+        when Hash
+          initialize_from_attributes_hash(attributes_or_record)
+        when self.class
+          initialize_from_record(attributes_or_record)
+        end
+      end
+
+      def initialize_from_record(record)
+        self.attributes = record.send(:attributes)
+      end
+
+      def to_record
+        rec = super
+        if rec["#{record_namespace}:customFieldList"]
+          rec["#{record_namespace}:customFieldList!"] = rec.delete("#{record_namespace}:customFieldList")
+        end
+        rec
+      end
+
+    end
+  end
+end
