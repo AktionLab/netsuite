bb1f2e3b33f8594e0390a83be9975839034bdb3b
Daniel Zukowski <daniel.zukowski@gmail.com>
2013-09-19 11:57:41 -0600

    PurchaseOrderExpenseList and PurchaseOrderExpense records

---
+++ lib/netsuite/records/purchase_order_expense_list.rb
@@ -0,0 +1,32 @@
+module NetSuite
+  module Records
+    class PurchaseOrderExpenseList
+      include Support::Fields
+      include Namespaces::TranPurch
+
+      fields :expense
+
+      def initialize(attributes = {})
+        initialize_from_attributes_hash(attributes)
+      end
+
+      def expense=(expenses)
+        case expenses
+        when Hash
+          self.expenses << PurchaseOrderExpense.new(expenses)
+        when Array
+          expenses.each { |expense| self.expenses << PurchaseOrderExpense.new(expense) }
+        end
+      end
+
+      def expenses
+        @expenses ||= []
+      end
+
+      def to_record
+        { "#{record_namespace}:expense" => expensess.map(&:to_record) }
+      end
+
+    end
+  end
+end
