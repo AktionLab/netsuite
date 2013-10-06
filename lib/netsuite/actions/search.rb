115a06f9c4aaf636c0f9c15509e2eafd6a2b6a65
Daniel Zukowski <daniel@ubuntu.(none)>
2013-10-05 19:34:13 -0600

    corrected urn for listAcct namespace

--- lib/netsuite/actions/search.rb
+++ lib/netsuite/actions/search.rb
@@ -29,7 +29,7 @@ module NetSuite
             'xmlns:platformMsgs' => "urn:messages_#{api_version}.platform.webservices.netsuite.com",
             'xmlns:platformCore' => "urn:core_#{api_version}.platform.webservices.netsuite.com",
             'xmlns:platformCommon' => "urn:common_#{api_version}.platform.webservices.netsuite.com",
-            'xmlns:listAcct' => "urn:relationships_#{api_version}.lists.webservices.netsuite.com",
+            'xmlns:listAcct' => "urn:accounting_#{api_version}.lists.webservices.netsuite.com",
             'xmlns:listRel' => "urn:relationships_#{api_version}.lists.webservices.netsuite.com",
             'xmlns:tranSales' => "urn:sales_#{api_version}.transactions.webservices.netsuite.com",
             'xmlns:setupCustom' => "urn:customization_#{api_version}.setup.webservices.netsuite.com"
